0926e96cfac26f6909bec2f461257096f313463c
David Reyes-Cairo <dreyescairo@gmail.com>
2013-10-12 01:14:29 -0600

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kikbook::Application.config.secret_token = 'dab815f546f42780753930a428ca56559fc2d03c3df1b9a4679622d92d580040842b9d99f3a4699268246679e9c94042e7012cd447649bdb77ce8c82da744f5b'
